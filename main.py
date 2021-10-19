7dae91c72b0e6a6188a3179c10f75251ca54008e
Egor Fedorenko <fedore.egor@gmail.com>
2021-10-19 22:04:31 +0300

    adding api url

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import requests
+import KeyHolder
+
 
 def get_city_name():
     print('Введите имя города по-английски')
     return input()
 
+
+def get_api_key():
+    return KeyHolder.get_key()
+
+
 def get_weather():
-    requests.get()
+    key = get_api_key()
+    city = get_city_name()
+    url = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key
+    resp = requests.get(url)
+
 
 if __name__ == '__main__':
-    city = get_city_name()
-
+    get_weather()
