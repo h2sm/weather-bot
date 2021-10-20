c7da5f619f0699c3031007fd283ac8c0225bc546
Egor Fedorenko <fedore.egor@gmail.com>
2021-10-20 14:07:05 +0300

    parsing json

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import KeyHolder
+import json
 
 
 def get_city_name():
@@ -15,13 +16,18 @@ def get_weather():
     key = get_api_key()
     city = get_city_name()
     url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&appid=" + key
+    print(url)
     resp = requests.get(url)
     if resp.status_code == 200:
         parse_response(resp)
+    else:
+        print('Неудачный запрос: ' + resp.status_code)
 
 
 def parse_response(resp):
-    print('e')
+    json_data = resp.json()
+    if 'weather' in json_data:
+        print(json_data["weather"])
 
 
 if __name__ == '__main__':
