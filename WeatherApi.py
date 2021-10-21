b8ea4f33078c2cd39eae67e396da7676f6c5abf7
Egor Fedorenko <fedore.egor@gmail.com>
2021-10-21 18:27:26 +0300

    bot works

--- WeatherApi.py
+++ WeatherApi.py
@@ -1,14 +1,16 @@
 import requests
 import KeyHolder
+
+
 def get_weather(cityname):
     key = KeyHolder.get_weather_api_key()
     city = cityname
     url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&appid=" + key
     resp = requests.get(url)
     if resp.status_code == 200:
-        parse_response(resp)
+        return parse_response(resp)
     else:
-        print("Неудачный запрос:" + str(resp.status_code))
+        return "Неудачный запрос:" + str(resp.status_code)
 
 
 def parse_response(resp):
@@ -17,8 +19,8 @@ def parse_response(resp):
         # print(i + " is a type of " + str(type(json_data[i])))
         if i == 'coord':
             coord_data = json_data[i]
-            lon = coord_data.get("lon")
-            lat = coord_data.get("lat")
+            lon = str(coord_data.get("lon"))
+            lat = str(coord_data.get("lat"))
         if i == 'weather':
             weather_data = json_data[i]
             list_weather_data = weather_data[0]
@@ -26,9 +28,13 @@ def parse_response(resp):
             weather_description = list_weather_data.get("description")
         if i == 'main':
             main_data = json_data[i]
-            temp = main_data.get("temp")
-            feels_like = main_data.get("feels_like")
-            min_temp = main_data.get("temp_min")
-            max_temp = main_data.get("temp_max")
+            temp = str(main_data.get("temp"))
+            feels_like = str(main_data.get("feels_like"))
+            min_temp = str(main_data.get("temp_min"))
+            max_temp = str(main_data.get("temp_max"))
         if i == 'name':
             city_name = json_data[i]
+
+    return "Weather in " + city_name + "(" + lon + "," + lat + "): " \
+           + main_weather + ", description - " + weather_description + ". Temp is " + temp + \
+           ", feels like " + feels_like + ", minimum is " + min_temp + ", max is " + max_temp
--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import KeyHolder
 import WeatherApi
 import telebot
+
 bot = telebot.TeleBot(KeyHolder.get_tg_key())
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.from_user.id,
+                     "Напишите название города, и я вам скажу какая там погода! Желательно по-английски")
 
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):  # getting name city
-    WeatherApi.get_weather(message.text)
+    weather_report = WeatherApi.get_weather(message.text)
+    bot.send_message(message.from_user.id, weather_report)
 
 
 bot.polling(none_stop=True, interval=0)
+
+if __name__ == '__main__':
+    start()
